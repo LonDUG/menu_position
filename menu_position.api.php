68f0b2d62e23489890e326c0efbee5b63da50027
JohnAlbin <virtually.johnalbin@gmail.com>
2010-10-21 16:52:15 +0000

    Major refactoring. (part 2)

---
+++ menu_position.api.php
@@ -0,0 +1,47 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Hooks provided by the Menu Position module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Registers rule plugins with menu_position module.
+ *
+ * Modules implementing menu position rule plugins should return an associative
+ * array of data. Each key in the array should be the name of plugin implemented
+ * by the module. Each value should be an associative array with the following
+ * options:
+ * - form_callback: (optional) The name of the function to use to add a
+ *   configurable form elements to the rule definition form. Defaults to
+ *   "MODULE_menu_position_rule_PLUGIN_form".
+ * - condition_callback: (optional) The name of the function to use when the
+ *   rule is being tested. Defaults to "MODULE_menu_position_condition_PLUGIN".
+ * - file: (optional) Include file containing form and condition callback
+ *   function definitions.
+ *
+ * @return
+ *   An associative array containing the information about each plugin.
+ */
+function hook_menu_position_rule_plugins() {
+  return array(
+    // Register the "foo" plugin.
+    'foo' => array(
+      'form_callback' => 'my_module_menu_position_rule_foo_form',
+      'condition_callback' => 'my_module_menu_position_condition_foo',
+      'file' => 'my_module.foo.inc',
+    ),
+    // Register the "bar" plugin.
+    'bar' => array(), // Use the defaults for all options.
+  );
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
