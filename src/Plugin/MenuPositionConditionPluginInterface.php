3023052da1a615c05919b281b621ab0ae28df114
Travis Neilans <travis@echdina.ca>
2016-05-26 21:31:02 -0400

    menu position condition interface

--- src/Plugin/MenuPositionConditionPluginInterface.php
+++ src/Plugin/MenuPositionConditionPluginInterface.php
@@ -8,12 +8,29 @@
 namespace Drupal\menu_position\Plugin;
 
 use Drupal\Component\Plugin\PluginInspectionInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Defines an interface for Menu position condition plugin plugins.
  */
 interface MenuPositionConditionPluginInterface extends PluginInspectionInterface {
 
-  // Add get/set methods for your plugin type here.
+  /**
+   * Returns the vertical tab from the condition
+   * @return array
+   *    The form element
+   */
+  public function getConditionForm(&$form, FormStateInterface $form_state);
+
+  /**
+   * Submit logic for each condition
+   */
+  public function conditionFormSubmit(&$form, FormStateInterface $form_state);
+
+  /**
+   * Whether or not to display the menu
+   * @return boolean
+   */
+  public function evaluateCondition($variables);
 
 }
