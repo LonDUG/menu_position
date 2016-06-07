6c6053080b045c8b1d871949cd5b8688fe585f0a
Luke Bainbridge <luke.bainbridge@gmail.com>
2016-06-07 19:52:59 -0400

    Removing development code

--- src/MenuPositionServiceProvider.php
+++ src/MenuPositionServiceProvider.php
@@ -9,8 +9,6 @@ use \Drupal\Core\DependencyInjection\ServiceProviderBase;
 class MenuPositionServiceProvider extends ServiceProviderBase{
 
   public function alter(ContainerBuilder $container) {
-    require_once('/Users/luke/Sites/menu-position/modules/contrib/devel/kint/kint/Kint.class.php');
-
     // Override the menu active trail with a new class.
     $definition = $container->getDefinition('menu.active_trail');
     $definition->setClass('Drupal\menu_position\Menu\MenuPositionActiveTrail');
